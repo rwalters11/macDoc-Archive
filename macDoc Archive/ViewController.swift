7a93fd006836d0fd8d91aeb5bc83aa83c592f9e6
Richard Walters <rwalters.flyer@icloud.com>
2019-12-29 14:00:32 +0000

    Initial Commit

---
+++ macDoc Archive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macDoc Archive
+//
+//  Created by Richard Walters on 29/12/2019.
+//  Copyright © 2019 Richard Walters. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
