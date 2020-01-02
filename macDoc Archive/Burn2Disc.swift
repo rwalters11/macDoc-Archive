524b72e4d774021409d64072d959996592386c69
Richard Walters <rwalters.flyer@icloud.com>
2020-01-02 16:03:45 +0000

    Disc Recording added

---
+++ macDoc Archive/Burn2Disc.swift
@@ -0,0 +1,20 @@
+//
+//  Burn2Disc.swift
+//  macDoc Archive
+//
+//  Created by Richard Walters on 02/01/2020.
+//  Copyright © 2020 Richard Walters. All rights reserved.
+//
+
+import Cocoa
+import DiscRecording
+import DiscRecordingUI
+
+class Burn2Disc: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
