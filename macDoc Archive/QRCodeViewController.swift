e5e9d3a744b172fd552ac1aa9c217de828c3850b
Richard Walters <rwalters.flyer@icloud.com>
2020-01-14 21:00:04 +0000

    QR code view

---
+++ macDoc Archive/QRCodeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  QRCodeViewController.swift
+//  macDoc Archive
+//
+//  Created by Richard Walters on 14/01/2020.
+//  Copyright © 2020 Richard Walters. All rights reserved.
+//
+
+import Cocoa
+
+class QRCodeViewController: NSViewController {
+
+    // Storyboard connections
+    @IBOutlet weak var imgViewQRCode: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
